7a24c834c169e934dd877d1ff9f203516d31bbca
unknown <x14125439@scr3-047.student.nci.col>
2015-04-14 20:34:28 +0100

    method nextChar added to main app class

--- GuessWord.java
+++ GuessWord.java
@@ -7,34 +7,60 @@ public class GuessWord{
     private boolean guessed[];//array of which indexes of word are already guessed
     private boolean lettersUsed[]; //what letters were passed to the object so far - to avoid repetition
     public GuessWord(String word){
-        this.word = word;
-        livesLost = 0;
-        attempts = 0;
-        kat = new Executioner(); //instance of an Executioner class.
+		//class instance constructor
+        this.word = word; //word to guess, has to be provided on init
+        livesLost = 0; //how many lives are lost, initially 0 of course
+        attempts = 0; //how many attempts were made to guess
+        kat = new Executioner(); //instance of an Executioner class (which displays hanging man).
         guessed = new boolean[word.length()];
         whatWasGused = new StringBuffer();
         for (int i=0;i<word.length();i++){
             whatWasGused.append("_"); //all positions of this variable are set to notguessed/unknown
             guessed[i]=false; //nothing was guessed on init
         }
+        //initialize array of used letters:
+        lettersUsed = new boolean[26];
+        for (int i=0; i<lettersUsed.length; i++){
+			lettersUsed[i] = false; //just in case, probably on init it's false anyway
+			//of course for mobile app I should not do it just in case.
+		}
     }
+    private boolean checkLetter(char letter){
+		//checks if letter was already used (true if used false if not)
+		//sets the letter as used if not
+		//I will use ascii character code to convert into index of my array of boleeans of used letters
+		int index = (int)letter - 97;
+		boolean returnValue = true; //I assume it was used already and will check if it's true in the next step.
+		if (lettersUsed[index] != true){
+			lettersUsed[index] = true;
+			returnValue = false;
+		}
+		return returnValue;
+	}
     public boolean takeAGuess(char letter){
-        boolean sthWasGuessed = false;
-        attempts+=1;
-        for (int i=0;i<word.length();i++){
-            if (word.charAt(i) == letter){
-                sthWasGuessed = true; //something guessed, no life will be taken
-                //set whatWasGused at this index to relevant letter:
-                whatWasGused.replace(i,i+1, Character.toString(letter));
-                //set guessed at this index to true:
-                guessed[i]=true;
-            }
-        }
-        if (!sthWasGuessed){
-            livesLost+=1;
-        }
-        return sthWasGuessed;
-    }
+		boolean sthWasGuessed = false;
+        //if letter wasn't used than check if it is in the word and other stuff:
+        if (!checkLetter(letter)){
+			//notice that the method checkLetter already took care of setting proper value in lettersUsed array
+			//this can be dangerous, I know, but I use this method only here and I set it to private
+        	attempts+=1;
+	        for (int i=0;i<word.length();i++){
+	            if (word.charAt(i) == letter){
+	                sthWasGuessed = true; //something guessed, no life will be taken
+	                //set whatWasGused at this index to relevant letter:
+	                whatWasGused.replace(i,i+1, Character.toString(letter));
+	                //set guessed at this index to true:
+	                guessed[i]=true;
+	            }
+	        }
+        	if (!sthWasGuessed){
+        	    livesLost+=1;
+        	}
+		}
+        //return if sth was guessed or not
+        //if the letter was already guessed it will still return false but no life will be taken
+	    return sthWasGuessed;
+	}
     public boolean isAlive(){
         if (livesLost>6){
             return false;
--- Hangman.java
+++ Hangman.java
@@ -1,10 +1,11 @@
+
 /*
 Main application class for HANGMAN project in JAVA
 Author:	Leszek Dubicki
 email:	Leszek.Dubicki@student.ncirl.ie
 ID:	x14125439
 */
-//import GuessMe.GuessMe;
+
 import java.util.Scanner;
 public class Hangman{
 	private static void clearscreen(){
@@ -12,6 +13,21 @@ public class Hangman{
 			System.out.print("\n");
 			//I'm clearing screen by displaying a lot of empty lines
 		}
+	}
+	public static char nextChar(Scanner Sc){
+		//takes nex char from the user
+		//it's actually missing method from Scanner class
+		//it will use scanner instance already initialized in main
+		String c;
+		int len;
+		do{
+			c = Sc.next();//get string
+			len = c.length(); //get length of it
+			if (len>1){
+				System.out.println("Please provide only one character!");
+			}
+		} while (len > 1); //Scanner already takes care of providing nothing at input
+		return c.charAt(0);
 	}
 	public static void main(String args[]){
 		Executioner E= new Executioner();
@@ -24,21 +40,22 @@ public class Hangman{
 		GuessWord Guesser;
 		//main application loop:
 		char answer = 'y';
-		while (answer == 'y'){
+		do {
 			word = W.getRandomWord(); //get random word from collection class
 			//System.out.println(word); //print the word for testing purpose
 			Guesser = new GuessWord(word); //initialize new guessing manager
 			while (Guesser.isAlive()){
 
 				System.out.print("Type a letter: ");
-				letter = ScanObj.next(".").charAt(0); //take exactly one char:
+				letter = nextChar(ScanObj);
+				//but it
 				clearscreen();
 				// System.out.println(letter); //print the letter for testing purpose
 				Guesser.takeAGuess(letter);
 				System.out.println(Guesser.displayStatus());
 			}
 
-		}
+		} while (answer == 'y');
 
 	}
 }
